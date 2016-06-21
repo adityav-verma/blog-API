539d49e1c6be22872b376fdacc2c052e2c34f23d
coderadi <aditya11.iiitm@gmail.como>
2016-06-21 15:53:44 +0530

    added api endpoints for a lot of resources

--- database.sql
+++ database.sql
@@ -5,6 +5,7 @@ CREATE TABLE users(
   password VARCHAR(255) NOT NULL,
   active INT NOT NULL,
   time_of_registration TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  role VARCHAR(100) NOT NULL DEFAULT "user",
   PRIMARY KEY (user_id),
   CONSTRAINT u_username UNIQUE(username),
   CONSTRAINT u_email UNIQUE(email)
@@ -18,4 +19,34 @@ CREATE TABLE session(
   login_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (session_id),
   FOREIGN KEY (username) REFERENCES users(username)
+);
+
+
+CREATE TABLE categories(
+  category_id INT AUTO_INCREMENT NOT NULL,
+  category_name VARCHAR(255) NOT NULL,
+  category_details TEXT NOT NULL,
+  PRIMARY KEY (category_id),
+  CONSTRAINT u_name UNIQUE(category_name)
+);
+
+CREATE TABLE blogs(
+  blog_id INT AUTO_INCREMENT NOT NULL,
+  user_id INT NOT NULL,
+  blog_title TEXT NOT NULL,
+  category_id INT NOT NULL,
+  blog_body TEXT NOT NULL,
+  published INT NOT NULL DEFAULT 0,
+  published_date TIMESTAMP,
+  creation_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY (user_id) REFERENCES users(user_id),
+  FOREIGN KEY (category_id) REFERENCES categories(category_id),
+  PRIMARY KEY (blog_id)
+);
+
+CREATE TABLE blog_images(
+  blog_id INT NOT NULL,
+  image_title VARCHAR(255),
+  image_path VARCHAR(255),
+  FOREIGN KEY (blog_id) REFERENCES blogs(blog_id)
 );
--- server.js
+++ server.js
@@ -5,9 +5,15 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var mysql = require("mysql");
 var md5 = require("md5");
+var fs = require("fs");
+var mv = require("node-mv");
+var fileUpload = require("express-fileupload");
+var path = require("path");
 
 //app instance
 var app = express();
+
+app.use(fileUpload());
 
 //mysql connection object
 var con = mysql.createConnection({
@@ -48,10 +54,45 @@ var logout = {
   "token": "authentication token"
 }
 
+var blog = {
+  "token": "token of the loggedIn user",
+  "user_id": "user id for the logged in user",
+  "category_id": "id of the category",
+  "blog_title": "title",
+  "blog_body": "body of the blog"
+};
+
+var category = {
+  "category_name": "Name of the category",
+  "category_details": "Details of the category"
+};
 
 //Router for the app
 var router = express.Router();
 
+//checks a valid token, returns -1 for invalid, 0 for normal user, 1 for admin
+function isAdmin(token, callback){
+  console.log(token);
+  var checkAdmin = "SELECT * FROM users INNER JOIN session on users.user_id = session.session_id WHERE token = ?";
+  var parameters = [token];
+  con.query(checkAdmin, parameters, function(err, data){
+    if(err){
+      callback(-1);
+    }
+    else{
+      if(data.length == 0){
+        console.log("inside data length 0");
+        callback(-1);
+      }
+      else{
+        if(data.role == "admin")
+          callback(1);
+        else
+          callback(0);
+      }
+    }
+  });
+}
 
 //loggin the requests
 router.use(function(req, res, next){
@@ -65,12 +106,14 @@ router.get('/', function(req, res) {
 });
 
 
+
+
 // ### Registration of a user
 router.route("/accounts/register")
   .post(function(req, res){
     if(!req.body.username || !req.body.email || !req.body.password){
       //request format checking
-      res.json(400, {"message": "Request Format Error", "required": register});
+      res.status(400).json({"message": "Request Format Error", "required": register});
     }
     else{
       var data = {"username":req.body.username, "password": md5(req.body.password), "email": req.body.email, "active": 1};
@@ -81,41 +124,67 @@ router.route("/accounts/register")
             "message": "Cannot Create User, Check Credentials",
             "error": err
           };
-          res.json(400, response);
+          res.status(400).json(response);
         }
         else {
-          res.json(201, {"message": "Successfully Created User"});
+          res.status(201).json({"message": "Successfully Created User"});
         }
       });
     }
 
   });
-
+router.route("/accounts/remove/:user_id")
+  .post(function(req, res){
+    var user_id = req.params.user_id;
+    var deactivateUser = "UPDATE users SET active = 0 WHERE user_id = ?";
+    var parameters = [user_id];
+    con.query(deactivateUser, parameters, function(err, data){
+      if(err){
+        var response = {
+          "message": "Cannot remove user",
+          "error": err
+        };
+        res.status(400).json(response);
+      }
+      else{
+        // TODO: Add checks whether a removed user already existed
+        var response = {
+          "message": "User removed"
+        };
+        res.status(200).json(response);
+      }
+    });
+  });
 // ### logging In A User
 router.route("/accounts/login")
   .post(function (req, res){
     if(!req.body.username || !req.body.password){
-      res.json(400, {"message": "Request Format Error", "required": login});
+      res.status(400).json({"message": "Request Format Error", "required": login});
     }
     else{
       var data = [req.body.username, md5(req.body.password)];
-      var loginQuery = "SELECT * FROM users WHERE user_id = ? AND password = ?";
+      var loginQuery = "SELECT * FROM users WHERE username = ? AND password = ?";
       con.query(loginQuery, data, function(err, data){
         if(err){
           var response = {
             "message": "Cannot Login User, Check Credentials",
             "error": err
           };
-          res.json(400, response);
+          res.status(400).json(response);
         }
         else{
           //generate a token
-          var token = md5((new Date()) + req.body.password);
+           if(data.length == 0){
+             res.status(400).json({"message": "No user with this username", "Data":data});
+           }
+           else{
+            var token = md5((new Date()) + req.body.password);
 
-          var saveToken = "INSERT INTO session SET ?";
-          con.query(saveToken, {"username": req.body.username, "token": token});
-          res.set("TOKEN", token);
-          res.json(200, {"message": "Successfully Aurthorized"});
+            var saveToken = "INSERT INTO session SET ?";
+            con.query(saveToken, {"username": req.body.username, "token": token});
+            //res.set("TOKEN", token);
+            res.status(200).json({"message": "Successfully Aurthorized", "token": token, "data": data});
+          }
         }
       });
     }
@@ -127,7 +196,7 @@ router.route("/accounts/login")
 router.route("/accounts/logout")
   .post(function (req, res){
     if(!req.body.username || !req.body.token){
-      res.json(400, {"message": "Request Format Error", "required": logout});
+      res.status(400).json({"message": "Request Format Error", "required": logout});
     }
     else{
       var logoutQuery = "UPDATE session SET ? WHERE username = ? AND token = ?"
@@ -137,14 +206,252 @@ router.route("/accounts/logout")
             "message": "Cannot Logout User, Check Credentials",
             "error": err
           };
-          res.json(400, response);
+          res.status(400).json(response);
         }
         else{
-          res.json(200, {"message": "Successfully Logged Out"});
+          res.status(200).json({"message": "Successfully Logged Out", "data": data});
         }
       });
 
     }
+  });
+
+router.route("/accounts/users")
+  .get(function(req, res){
+    var allUsers = "SELECT user_id, username, email, active, role, time_of_registration FROM users";
+    con.query(allUsers, function(err, data){
+      if(err){
+        var response = {
+          "message": "Cannot list users",
+          "error": err
+        };
+        res.status(400).json(response);
+      }
+      else{
+        res.status(200).json(data);
+      }
+    });
+
+  });
+
+//tesign routes
+router.route("/test/")
+  .post(function(req, res){
+    var value = isAdmin(req.body.token, function(data){
+      console.log(data);
+      res.json({"value": data});
+    });
+  });
+
+// ### Adding categories
+router.route("/blogs/categories")
+  .post(function(req, res){
+    if(!req.body.category_name || !req.body.category_details){
+      res.status(400).json({"message": "Request Format Error", "required": category});
+    }
+    else{
+      var data = {"category_name": req.body.category_name, "category_details": req.body.category_details};
+      var addCategory = "INSERT INTO categories SET ?";
+      con.query(addCategory, data, function(err, data){
+        if(err){
+          var response = {
+            "message": "Cannot add category",
+            "error": err
+          };
+          res.status(400).json(response);
+        }
+        else{
+          res.status(201).json({"message": "Created Category Successfully"});
+        }
+      });
+    }
+  })
+  .get(function(req, res){
+    var allCategories = "SELECT * FROM categories";
+    con.query(allCategories, function(err, data){
+      if(err){
+        var response = {
+          "message": "Cannot show categories",
+          "error": err
+        }
+        res.status(400).json(response);
+      }
+      else{
+        res.status(200).json(data);
+      }
+    });
+  });
+// ##### ADDING NEW BLOGS ####
+router.route("/blogs")
+  .post(function(req, res){
+    if(!req.body.blog_title || !req.body.blog_body || !req.body.category_id || !req.body.user_id){
+      res.status(400).json({"message": "Request Format Error", "required": blog});
+    }
+    else{
+      var addBlog = "INSERT INTO blogs SET ?";
+      var data = {"blog_title": req.body.blog_title, "blog_body": req.body.blog_body, "category_id": req.body.category_id, "user_id": req.body.user_id};
+      con.query(addBlog, data, function(err, data){
+        if(err){
+          var response = {
+            "message": "Cannot add blog",
+            "error": err
+          };
+          res.status(400).json(response);
+        }
+        else{
+          var getBlog = "SELECT * FROM blogs WHERE user_id = ? ORDER BY blog_id DESC LIMIT 1";
+          con.query(getBlog, [req.body.user_id], function(err, data){
+            if(err){
+              res.json(err);
+            }
+            else{
+              res.json(data[0]);
+            }
+          });
+        }
+      });
+    }
+  })
+  .get(function(req, res){
+    //var getBlogs = "SELECT * FROM blogs INNER JOIN blogs_images on blogs.blog_id = blog_images.blog_id INNER JOIN users on blogs.user_id = users.user_id";
+    var getBlogs = "SELECT * FROM blogs LEFT OUTER JOIN blog_images on blogs.blog_id = blog_images.blog_id INNER JOIN users on blogs.user_id = users.user_id INNER JOIN categories ON categories.category_id = blogs.category_id";
+    con.query(getBlogs, function(err, data){
+      if(err){
+        var response = {
+          "message": "Cannot show blogs",
+          "errors": err
+        };
+        res.json(response);
+      }
+      else{
+        res.json(data);
+      }
+    });
+  });
+
+router.route("/blogs/images")
+  .post(function(req, res){
+    var blogImage;
+    if(!req.query.blog_id){
+      res.status(400).json({"message": "Bad request!"});
+      return;
+    }
+    var blog_id = req.query.blog_id;
+    if(!req.files){
+      var addDefaultImage = "INSERT INTO blog_images SET ?";
+      var data = {"blog_id": req.query.blog_id, "image_title": "default", "image_path": "images/default.jpeg"};
+      con.query(addDefaultImage, data, function(err, data){
+        if(err){
+          var response = {
+            "message": "Cannot add default image",
+            "error": err
+          };
+          res.json(response);
+        }
+        else{
+          res.status(201).json({"message": "Added default image"});
+        }
+      });
+    }
+    else{
+      blogImage = req.files.blogImage;
+      image_title = md5(new Date());
+      var url = "images/" +  image_title + ".jpeg";
+      blogImage.mv(url, function(err){
+        if (err) {
+    			res.status(500).send(err);
+    		}
+    		else {
+          var addImage = "INSERT INTO blog_images SET ?";
+          console.log(blog_id);
+          var data = {"blog_id": blog_id, "image_title": image_title, "image_path": url};
+          con.query(addImage, data, function(err, data){
+            if(err){
+              res.json(err);
+            }
+            else{
+              res.json({"message": "Image uploaded", "path": url});
+            }
+          });
+    		}
+      });
+    }
+  })
+
+//tesing API for image upload
+router.route("/upload")
+  .post(function(req, res){
+    var sampleFile;
+
+  	if (!req.files) {
+  		res.send('No files were uploaded.');
+  		return;
+  	}
+
+  	sampleFile = req.files.sampleFile;
+    console.log();
+  	sampleFile.mv('upload/a.jpeg', function(err) {
+  		if (err) {
+  			res.status(500).send(err);
+  		}
+  		else {
+  			res.send('File uploaded!');
+  		}
+  	});
+  });
+
+router.route("/blogs/publish/:blog_id")
+  .post(function(req, res){
+    var blog_id = req.params.blog_id;
+    var publishBlog = "UPDATE blogs SET published = 1, published_date = NOW() WHERE blog_id = ?";
+    var parameters = [blog_id];
+    con.query(publishBlog, parameters, function(err, data){
+      if(err){
+        var response = {
+          "message": "Cannot publish Blog",
+          "error": err
+        };
+
+        res.status(400).json(response);
+      }
+      else{
+        // TODO: Solve issue of server crashing when applying these conditions
+        // if(data["affectedRows"] == 0){
+        //   res.status(400).json({"message": "No blog with this blog id", "data": data});
+        // }
+        // else{
+        //   res.satus(200).json({"message": "Blog Published", "data": data});
+        // }
+        res.json(data);
+      }
+    });
+  });
+
+router.route("/blogs/depublish/:blog_id")
+  .post(function(req, res){
+    var blog_id = req.params.blog_id;
+    var publishBlog = "UPDATE blogs SET published = 0, published_date = NOW() WHERE blog_id = ?";
+    var parameters = [blog_id];
+    con.query(publishBlog, parameters, function(err, data){
+      if(err){
+        var response = {
+          "message": "Cannot De publish Blog",
+          "error": err
+        };
+
+        res.status(400).json(response);
+      }
+      else{
+        // TODO: Solve issue of server crashing when applying these conditions
+        // if(data["affectedRows"] == 0){
+        //   res.status(400).json({"message": "No blog with this blog id", "data": data});
+        // }
+        // else{
+        //   res.satus(200).json({"message": "Blog Published", "data": data});
+        // }
+        res.json(data);
+      }
+    });
   });
 //all the endpoints will be prefixed with /api/
 app.use("/api", router);
